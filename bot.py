ff27d9834342f0051fb60efadb629f15e619f42b
ochoatanny845-ops <ochoatanny845-ops@users.noreply.github.com>
2026-04-15 16:39:42 +0800

    修复自定义按钮交互式输入无响应：添加消息处理路由

--- bot.py
+++ bot.py
@@ -443,7 +443,7 @@ class SalesBot:
         # 优先处理管理员输入
         admin_waiting = context.user_data.get('admin_waiting_for')
         if admin_waiting:
-            if admin_waiting in ['trc20_address', 'start_message']:
+            if admin_waiting in ['trc20_address', 'start_message', 'custom_button_text', 'custom_button_message', 'custom_button_url']:
                 handled = await self.admin_handler.handle_admin_message(update, context)
                 if handled:
                     return
